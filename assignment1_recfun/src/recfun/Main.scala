df7f7676a7ca6f833da2bc176b104f41b5db7371
Petro Karabyn <karabyn@ucu.edu.ua>
2018-05-06 18:36:22 +0300

    Implement Exercise 2: Parentheses Balancing

--- assignment1_recfun/src/recfun/Main.scala
+++ assignment1_recfun/src/recfun/Main.scala
@@ -6,12 +6,21 @@ package recfun
   */
 object Main {
   def main(args: Array[String]) {
+
     println("Pascal's Triangle")
     for (row <- 0 to 10) {
       for (col <- 0 to row)
         print(pascal(col, row) + " ")
       println()
     }
+
+    println("Balancer")
+    println(balance("(if (zero? x) max (/ 1 x))".toList))
+    println(balance("I told him (that it's not (yet) done).\n(But he wasn't listening)".toList))
+    println(balance(":-)".toList))
+    println(balance("())(".toList))
+    println(balance(" ( () () )".toList))
+    println(balance("((()))(".toList))
   }
 
   /**
@@ -31,7 +40,22 @@ object Main {
   /**
     * Exercise 2
     */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def diff(chars: List[Char], delta: Int): Int = {
+      if(chars.isEmpty) return delta
+      val char = chars.head
+      if(char == '(') {
+        diff(chars.tail, delta + 1)
+      } else if(char == ')') {
+        if(delta == 0) delta - 1
+        else diff(chars.tail, delta - 1)
+      } else {
+        diff(chars.tail, delta)
+      }
+    }
+
+    diff(chars, 0) == 0
+  }
 
   /**
     * Exercise 3
